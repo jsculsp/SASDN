08d0f8dd789bc9d7ae7c13646f1842f4fae4a5f7
liushengpeng <328232234@qq.com>
2018-07-05 18:08:29 +0800

    update joi fix bug

--- scaffold/src/lib/ProtobufJoiValidation.ts
+++ scaffold/src/lib/ProtobufJoiValidation.ts
@@ -6,14 +6,14 @@ export const MIN_INT32_VALUE = -2147483648;
 export namespace ProtobufJoiValidation {
   export const vDouble = joi.extend((joi) => ({
     base: joi.number().min(Number.MIN_SAFE_INTEGER).max(Number.MAX_SAFE_INTEGER),
-    name: 'activate'
+    name: 'activate',
   }));
 
   export const vFloat = vDouble;
 
   export const vInt32 = joi.extend((joi) => ({
     base: joi.number().integer().min(MIN_INT32_VALUE).max(MAX_INT32_VALUE),
-    name: 'activate'
+    name: 'activate',
   }));
 
   export const vSint32 = vInt32;
@@ -24,7 +24,7 @@ export namespace ProtobufJoiValidation {
 
   export const vInt64 = joi.extend((joi) => ({
     base: joi.number().integer().min(Number.MIN_SAFE_INTEGER).max(Number.MAX_SAFE_INTEGER),
-    name: 'activate'
+    name: 'activate',
   }));
 
   export const vSint64 = vInt64;
@@ -32,26 +32,26 @@ export namespace ProtobufJoiValidation {
   export const vSfixed64 = vInt64;
 
   export const vUint32 = joi.extend((joi) => ({
-    base: joi.number().integer().positive().max(MAX_INT32_VALUE).allow(0),
-    name: 'activate'
+    base: joi.number().integer().max(MAX_INT32_VALUE).min(0),
+    name: 'activate',
   }));
 
   export const vFixed32 = vUint32;
 
   export const vUint64 = joi.extend((joi) => ({
-    base: joi.number().integer().positive().max(Number.MAX_SAFE_INTEGER).allow(0),
-    name: 'activate'
+    base: joi.number().integer().max(Number.MAX_SAFE_INTEGER).min(0),
+    name: 'activate',
   }));
 
   export const vFixed64 = vUint64;
 
   export const vBool = joi.extend((joi) => ({
     base: joi.bool(),
-    name: 'activate'
+    name: 'activate',
   }));
 
   export const vString = joi.extend((joi) => ({
     base: joi.string(),
-    name: 'activate'
+    name: 'activate',
   }));
 }
